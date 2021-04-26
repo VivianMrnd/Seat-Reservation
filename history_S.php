e125d44d562151c160381ba099c21692942bd79d
nalcsmn <nlacsamana09@gmail.com>
2021-04-26 21:56:54 +0800

    add Css

--- history_S.php
+++ history_S.php
@@ -1,28 +1,52 @@
-<?php
-  require_once 'php/init.php';
-?>
 
+<?php
+require_once 'php/init.php';
+?>
+<!doctype html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/history.css" />
+    <link rel="icon" href="img/favicon.ico" />
     <title>SEAT RESERVATION HISTORY</title>
+
 </head>
-<style>
-    .history{
-        height: 500px;
-        overflow-x: hidden;
-    }
-</style>
-<body class='container'>
-    <div class="history mt-md-5">
-        <h1 class='text-center'>History</h1>
+
+<body>
+    <section class="heder" id="title">
+        <div class="container-fluid navcon">
+            <div class="container">
+                <nav class="navbar navbar-expand-md  pt-md-2 mb-5">
+                    <img src="img/logo.png" class="img-fluid logo" />
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ml-auto">
+                            <a class="nav-item nav-link active navitem" href="index.php">Home <span class="sr-only"></span></a>
+
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="container gallery-container">
+           <div class="history mt-md-5">
+           <h1 class='text-center'>History</h1>
         <h4 class='text-center'>Movie: Spongebob: the missing spatula movie</h4>
         <?php  SSP_History(); ?>
     </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
--- spongebob.php
+++ spongebob.php
@@ -1,77 +1,96 @@
 <?php
-  require_once 'php/init.php';
+require_once 'php/init.php';
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/booking.css" />
+    <link rel="icon" href="img/favicon.ico" />
 
-    <title>SEAT RESERVATION - Spongebob</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100&family=Montserrat:ital@1&display=swap" rel="stylesheet">
+    <title>SEAT RESERVATION </title>
     <style>
-        .grid-container {
-        display: grid;
-        grid-template-columns: auto auto auto;
-        background-color: #2196F3;
-        padding: 5px;
-        }
-        .grid-item {
-        background-color: rgba(255, 255, 255, 0.8);
-        border: 1px solid rgba(0, 0, 0, 0.8);
-        padding: 5px;
-        font-size: 30px;
-        text-align: center;
-        }
+
     </style>
 </head>
-  </head>
-  <body class="container">
-      <h1>Spongebob: the missing spatula movie</h1>
-    <a class="btn btn-info mt-md-5 text-center" href= history_S.php target=_blank><h4>View History</h4></a>
-    <div class="row">
-        <div class="col-md-3 mt-md-5">
-                <?php 
-                    insertR_S(); 
-                    showForm_S(); 
-                ?>
-        </div>
-        <div class='col-md-9 mt-md-5'>
-            <div class='row'>
-                <div class='col-md-4'>
-                    <h2>BOX A</h2>
-                    <div class='grid-container'>
-                        <div class='grid-item'>
-                            <?php viewBoxA_S(); ?>
+</head>
+
+<body>
+    <section class="heder" id="title">
+        <div class="container-fluid navcon">
+            <div class="container">
+                <nav class="navbar navbar-expand-md  pt-md-2 mb-5">
+                    <img src="img/logo.png" class="img-fluid logo" />
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ml-auto">
+                            <a class="nav-item nav-link active navitem" href="index.php">Home <span class="sr-only"></span></a>
+
                         </div>
                     </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="container gallery-container">
+            <h1>The Spongebob Movie</h1>
+            <div class="text-center">
+                <a class="btn btn-info mt-md-5 text-center btnbok" href=history.php target=_blank>
+                    <h4>View History</h4>
+                </a>
+            </div>
+            <div class="row">
+                <div class="col-md-3 mt-md-5">
+                    <?php
+                    insertR_S();
+                    showForm_S();
+                    ?>
                 </div>
-                <div class='col-md-4'>
-                    <h2>BOX B</h2>
-                    <div class='grid-container'>
-                        <div class='grid-item'>
-                            <?php viewBoxB_S(); ?>
-                        </div>                   
+                <div class='col-md-9 mt-md-5'>
+                    <div class='row'>
+                        <div class='col-md-4'>
+                            <h2>BOX A</h2>
+                            <div class='grid-container'>
+                                <div class='grid-item'>
+                                    <?php viewBoxA_S(); ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='col-md-4'>
+                            <h2>BOX B</h2>
+                            <div class='grid-container'>
+                                <div class='grid-item'>
+                                    <?php viewBoxB_S(); ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='col-md-4'>
+                            <h2>BOX C</h2>
+                            <div class='grid-container'>
+                                <div class='grid-item'>
+                                    <?php
+                                    viewBoxC_S();
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class='col-md-4'>
-                    <h2>BOX C</h2>
-                    <div class='grid-container'>
-                        <div class='grid-item'>
-                            <?php 
-                                viewBoxC_S();  
-                            ?>
-                        </div> 
-                    </div>
+
                 </div>
             </div>
-            
-        </div>
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    
-    </body>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+
 </html>
